cdadf4545639f90551801751f5187c7d1c4ac5a5
teruh <zachclarkbyte@gmail.com>
2020-04-17 21:09:11 -0400

    Initial commit

---
+++ desktop/src/me/zacl/ld46/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.zacl.ld46.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.zacl.ld46.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
